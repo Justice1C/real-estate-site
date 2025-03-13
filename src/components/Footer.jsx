abb94cfb4f43852dd7277660bf515bb2a8d52959
Justice1C <justicec566@gmail.com>
2025-03-13 21:00:38 +0100

    contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDarkMode } from './DarkModeContext';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 const Footer = () => {
+    useEffect(() => {
+      AOS.init({
+        offset: 200,
+        duration: 800,
+        easing: "ease-in-sine",
+        delay: 100,
+      });
+    }, []);
+  
+    const { darkMode, toggleDarkMode } = useDarkMode();
+    console.log("toggleDarkMode", toggleDarkMode);
   return (
     <div>
       
